1120878b052379945ea9a7dffa6b99c8586333ec
Lassana DIAKITE <lassanadiakite90@gmail.com>
2021-06-13 22:06:06 +0200

    OrderItem entity cascade persit

--- src/Entity/OrderItem.php
+++ src/Entity/OrderItem.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\OrderItemRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
  * @ApiResource()
